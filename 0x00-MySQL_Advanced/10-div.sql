a0ce8d69a1a76c01c92114bbd46f753927c65db5
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-05-05 17:11:05 +0000

    late work

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,11 +3,12 @@
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
     IF b != 0 THEN
         SET result = a / b;
-    ELSE
-            SET result = 0;
     END IF;
     RETURN result;
 END $$
